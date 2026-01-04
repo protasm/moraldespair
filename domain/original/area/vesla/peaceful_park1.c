11931230e69b82c532075e2ef77d27e0df647ce0
jonathan <jon.stroble@gmail.com>
2026-01-04 02:08:46 +0000

    devel/refactor

---
+++ domain/original/area/vesla/peaceful_park1.c
@@ -0,0 +1,9 @@
+#include "room.h"
+
+FOUR_EXIT("domain/original/area/vesla/sanctuary", "north",
+	  "domain/original/area/vesla/peaceful_park2", "south",
+	  "domain/original/area/vesla/sanctuary", "east",
+	  "domain/original/area/vesla/sanctuary", "west",
+	  "A peaceful park",
+	  "A peaceful park.\n",
+	  1)
--- domain/original/area/vesla/sanctuary.c
+++ domain/original/area/vesla/sanctuary.c
@@ -3,19 +3,22 @@
 int reboot_time, time_from_reset, last_reset_cycle;
 int list_length;
 
-void reset(string arg)
-{
+void reset(string arg) {
     if (time_from_reset)
-	last_reset_cycle = time() - time_from_reset;
+        last_reset_cycle = time() - time_from_reset;
+
     time_from_reset = time();
+
     if (arg)
-	return;
+        return;
+
     set_light(1);
+
     reboot_time = time();
 }
 
-void init()
-{
+void init() {
+    add_action("south", "south");
 }
 
 string short() {
@@ -25,46 +28,78 @@ string short() {
 void long(string str)
 {
     if (str == "clock") {
-	int i, j;
-	write("The clock shows ");
-	i = time() - reboot_time;
-	j = i / 60 / 60 / 24;
-	if (j == 1)
-	    write("1 day ");
-	else if (j > 0)
-	    write(j + " days ");
-	i -= j * 60 * 60 * 24;
-	j = i / 60 / 60;
-	if (j == 1)
-	    write("1 hour ");
-	else if (j > 0)
-	    write(j + " hours ");
-	i -= j * 60 * 60;
-	j = i / 60;
-	if (j == 1)
-	    write("1 minute ");
-	else if (j > 0)
-	    write(j + " minutes ");
-	i -= j * 60;
-	if (i == 1)
-	    write("1 second");
-	else if (i > 0)
-	    write(i + " seconds");
-	write("\n");
-	if (this_player()->query_level() < 20)
-	    return;
-	write("Time since reset is " + (time() - time_from_reset) +
-	      " seconds.\n");
-	if (last_reset_cycle)
-	    write("Reset cycle: " + last_reset_cycle + "\n");
-	write("Free block list length: " + list_length + "\n");
-	return;
-    }
-    write("The Sanctuary.\n");
-    write("There is a clock on the wall.\n");
+        int i, j;
 
+        write("The clock shows ");
+
+        i = time() - reboot_time;
+        j = i / 60 / 60 / 24;
+
+        if (j == 1)
+            write("1 day ");
+        else if (j > 0)
+            write(j + " days ");
+
+        i -= j * 60 * 60 * 24;
+        j = i / 60 / 60;
+
+        if (j == 1)
+            write("1 hour ");
+        else if (j > 0)
+            write(j + " hours ");
+
+        i -= j * 60 * 60;
+        j = i / 60;
+
+        if (j == 1)
+            write("1 minute ");
+        else if (j > 0)
+            write(j + " minutes ");
+
+        i -= j * 60;
+
+        if (i == 1)
+            write("1 second");
+        else if (i > 0)
+            write(i + " seconds");
+
+        write("\n");
+
+        if (this_player()->query_level() < 20)
+            return;
+
+        write("Time since reset is " + (time() - time_from_reset) +
+              " seconds.\n");
+
+        if (last_reset_cycle)
+            write("Reset cycle: " + last_reset_cycle + "\n");
+
+        write("Free block list length: " + list_length + "\n");
+
+        return;
+    }
+
+    write(short() + "\n");
 }
 
 int id(string str) {
     return str == "clock";
+}
+
+int pray() {
+    return this_player()->remove_ghost();
+}
+
+int prevent_look_at_inv(string str) {
+    return str != 0;
+}
+
+int south() {
+    this_player()->move_player("south#domain/original/area/vesla/peaceful_park1");
+
+    return 1;
+}
+
+int query_drop_castle() {
+    return 1;
 }
