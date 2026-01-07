ba107a95e1554796c05e0f4c81976687ee1a988a
jonathan <jon.stroble@gmail.com>
2026-01-07 00:17:18 +0000

    devel/refactor

--- domain/original/area/vesla/portal.c
+++ domain/original/area/vesla/portal.c
@@ -10,7 +10,7 @@ void reset(int arg) {
     long_desc = "Portal Room.\n";
     dest_dir = ({
         "domain/original/area/vesla/sanctuary", "up",
-        "domain/original/area/balin/room605", "island",
+        "domain/original/area/island/room605", "island",
         "domain/original/area/preserve/room432", "preserve",
         "domain/original/area/forest/room529", "forest",
         "domain/original/area/pylus/room1205", "pylus",
