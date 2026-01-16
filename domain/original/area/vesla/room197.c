502200e6f2fe2f1f02d4440898e2b6ea1af26b9c
jonathan <jon.stroble@gmail.com>
2026-01-16 01:09:14 +0000

    devel/refactor

--- domain/original/area/vesla/room197.c
+++ domain/original/area/vesla/room197.c
@@ -8,7 +8,7 @@ void reset(int arg) {
   set_light(1);
 
   short_desc = "Abandoned Corner";
-  long_desc = "Rapier Way meets a broader boulevard on a patch of sunken stone and grit here.\n"
+  long_desc = "A worn way meets the broader boulevard on a patch of sunken stone and grit here.\n"
               + "The junction is scarred by ruts and hollows, with grit swept into the corners.\n";
   dest_dir = ({
     "domain/original/area/vesla/room196", "west",
