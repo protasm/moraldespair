77de9acd5bdd580f7ddd122f21f7b13bfb0ee9dc
MrMe <134198354+mrhvandenoever@users.noreply.github.com>
2026-01-18 22:39:51 +0100

    Fix long_desc assignment in room611.c

--- domain/original/area/island/room611.c
+++ domain/original/area/island/room611.c
@@ -7,8 +7,7 @@ void reset(int arg) {
     set_light(1);
 
     short_desc = "Silver Street";
-    long_desc = long_desc = 
-      "Smooth, pale stone lines this wide thoroughfare under a stagnant sky.\n" +
+    long_desc = "Smooth, pale stone lines this wide thoroughfare under a stagnant sky.\n" +
       "Dead metallic torches stand in silent intervals, their glow long since\n" +
       "extinguished. To the east, an ornate building rises toward the heavens\n" +
       "with twin, crumbling spires. Opposite the spires, a nameless storefront\n" +
