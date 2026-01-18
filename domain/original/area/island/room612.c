4934528b11678c65768c61617fb0b8ef9761b9ac
maxpecks <mrhvandenoever@gmail.com>
2026-01-18 22:23:31 +0100

    fixed error

--- domain/original/area/island/room612.c
+++ domain/original/area/island/room612.c
@@ -1,20 +1,23 @@
 inherit "room/room";
 
 void reset(int arg) {
-    if (arg)
-        return;
+  if (arg) {
+    return;
+  }
 
-    set_light(1);
+  set_light(1);
+  short_desc = "Artisan Row";
+  long_desc = 
+    "The pale stones of the street remain eerily clear of debris, as if the\n" +
+    "city itself refuses to decay. To the west, a massive, cold furnace marks\n" +
+    "the remains of a foundry. The eastern storefront, once smelling of grain\n" +
+    "and yeast, now exhales only the scent of damp flour and rot. Northward,\n" +
+    "the shadows of monumental structures loom over the cooling metal torches.\n";
 
-    short_desc = "Silver Street";
-    long_desc = "The pale stones of the street remain eerily clear of debris, as if the\n" +
-      "city itself refuses to decay. To the west, a massive, cold furnace marks\n" +
-      "the remains of a foundry. The eastern storefront, once smelling of grain\n" +
-      "and yeast, now exhales only the scent of damp flour and rot. Northward,\n" +
-      "the shadows of monumental structures loom over the cooling metal torches.\n
-    dest_dir = ({
-        "domain/original/area/island/room611", "south",
-        "domain/original/area/island/room674", "east",
-        "domain/original/area/island/room613", "north",
-    });
+  dest_dir = ({
+    "domain/original/area/island/room613", "north",
+    "domain/original/area/island/room674", "east",
+    "domain/original/area/island/room611", "south",
+    "domain/original/area/island/room642", "west",
+  });
 }
