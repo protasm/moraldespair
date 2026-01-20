c27c22fad53487c646e546ceeae077f0469fe000
jonathan <jon.stroble@gmail.com>
2026-01-20 12:01:04 +0000

    remove

--- secure/master_old.c
+++
@@ -1,7 +0,0 @@
-/*  secure/master_old.c     (plain default)
-**
-** Since LP-245 is a compat mudlib, this master file is just a wrapper
-** around the real master, /secure/master.c
-*/
-
-#include "/secure/master.c"
