0d4191f79cb8979b904d2f2564ac984c0034c12a
protasm <36781079+protasm@users.noreply.github.com>
2026-01-23 11:11:56 -0500

    Allow chapter objects to save

--- secure/master.c
+++ secure/master.c
@@ -1566,6 +1566,10 @@ mixed valid_write (string path, string euid, string fun, object caller) {
         if (user[0..3] == "obj/"
          || user[0..3] == "std/")
           return ADD_SLASH(path);
+        if (sizeof(user) > 7 && user[0..7] == "chapter/") {
+          if (sizeof(path) > 7 && path[0..7] == "chapter/")
+            return ADD_SLASH(path);
+        }
         if (sizeof(user) > 12
           && user[0..12] == "secure/player"
           && path[0..7] == "players/")
