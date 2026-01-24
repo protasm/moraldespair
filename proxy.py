7a435394319e5dc0fc1b7e737de496ce5c3b7ca0
protasm <36781079+protasm@users.noreply.github.com>
2026-01-24 13:06:07 -0500

    Fix ERQ TCP response handling

--- proxy.py
+++ proxy.py
@@ -48,6 +48,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
         self.send_header("Content-Length", str(len(out)))
+        self.send_header("Connection", "close")
         self.end_headers()
 
         self.wfile.write(out)
