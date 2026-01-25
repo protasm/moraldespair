ee1eab8b71d5a81210d96ab8a5ec9b5832af9bc0
jonathan <jon.stroble@gmail.com>
2026-01-25 02:23:20 +0000

    devel/refactor

--- include/globals.h
+++ include/globals.h
@@ -1,36 +1,16 @@
-// file: globals.h
+#define LOG_DIR "/log"
 
-#ifdef __SENSIBLE_MODIFIERS__
-#define staticf protected
-#define staticv nosave
-#else
-#define staticf static
-#define staticv static
-#endif
-
-#include "tests.h"
-
-#define SINGLE_DIR "/single"
-#define CONFIG_DIR "/etc"
-#define LOG_DIR    "/log"
-
-#define VIRTUAL_D  "/single/virtual"
-
-#define LOGIN_OB   "/clone/login"
-#define USER_OB    "/clone/user"
-#define VOID_OB    "/single/void"
+#define LOGIN_OB "/core/login"
+#define PLAYER_OB "/core/player"
+#define VOID_OB "/core/void"
 #define START_ROOM "/chapter/prologue/area/ruined/sanctuary"
 
-#define ROOT_UID     "Root"
+#define ROOT_UID "Root"
 #define BACKBONE_UID "Backbone"
 
-#define BASE            "/inherit/base"
-#define OVERRIDES_FILE  "/single/simul_efun"
+#define BASE_FILEE "/inherit/base"
+#define OVERRIDES_FILE "/core/simul_efun"
 
 #define COMMAND_PREFIX "/command/"
-
-// classes for message() efun.
-#define M_STATUS "status"
-#define M_SAY    "say"
 
 #define CLEAN_UP "/inherit/clean_up"
