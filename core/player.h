58aafcdfc40a2c194296aa7ca43e039fb93883a1
protasm <36781079+protasm@users.noreply.github.com>
2026-01-26 20:04:13 -0500

    Add account and avatar runtime models

--- core/player.h
+++ core/player.h
@@ -4,3 +4,7 @@ string query_name();
 void set_name(string new_name);
 string query_account();
 void set_account(string new_account);
+object query_curr_avatar();
+void set_curr_avatar(object avatar);
+string query_account_email();
+int set_account_email(string new_email);
--- include/globals.h
+++ include/globals.h
@@ -8,13 +8,15 @@
 
 #define LOGIN_OB "/core/login"
 #define PLAYER_OB "/core/player"
+#define AVATAR_OB "/core/avatar"
 #define VOID_OB "/core/void"
 
 #define ACCOUNT_D "/daemon/account_d"
 
 #define START_ROOM "/chapter/prologue/area/ruined/room/sanctuary"
 
-#define COMMAND_PREFIX "/chapter/prologue/command/"
+#define COMMAND_PREFIX "/command/"
+#define ACTION_PREFIX "/chapter/prologue/action/"
 
 #define PLAYER_PROMPT "> "
 
