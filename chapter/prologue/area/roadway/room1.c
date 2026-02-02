0a9874731dc80f4004ef8ae60ed7779f039e8983
jonathan <jon.stroble@gmail.com>
2026-02-02 02:37:46 +0000

    devel/refactor

--- chapter/prologue/area/roadway/room1.c
+++ chapter/prologue/area/roadway/room1.c
@@ -3,14 +3,12 @@ inherit "/chapter/prologue/std/room";
 void create() {
   ::create();
 
-  short_desc = "Walking on a roadway";
-  long_desc = "Walking on a roadway.\n";
+  short_desc = "Roadway";
+  long_desc = "Walking on a roadway, east of the remains of a sand-drowned stronghold.";
 //   dest_dir = ([
 //     "west":"/chapter/prologue/area/sunken/entrance",
 //     "east":"/chapter/prologue/area/roadway/room2",
 //     "north":"/chapter/prologue/std/wilderness_room#L27",
 //     "south":"/chapter/prologue/std/wilderness_room#L29",
 //   ]);
-
-  set_light(1);
 }
