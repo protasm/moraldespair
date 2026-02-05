ad28d25521a308ec37db28ed3f62f37933e94213
protasm <36781079+protasm@users.noreply.github.com>
2026-02-04 21:02:22 -0500

    Add dead area link definitions

--- chapter/prologue/area/dead/room1595.c
+++ chapter/prologue/area/dead/room1595.c
@@ -5,10 +5,12 @@ void create() {
 
   short_desc = "Cold Narrow Way";
   long_desc = "Pitted stone stretches ahead, littered with chips and fragments. Blackened streaks and gouges run along the masonry, breaking any clean line. The way runs on in a narrow line, hemmed by broken walls left to weather. Thin lichen mats the shaded edges, and the air tastes of wet dust.";
+  /*
   dest_dir = ([
     "east" : "/chapter/prologue/area/dead/room1594",
     "west" : "/chapter/prologue/area/dead/room1596",
   ]);
+  */
 
   set_light(1);
 }
--- chapter/prologue/area/dead/room1623.c
+++ chapter/prologue/area/dead/room1623.c
@@ -5,8 +5,10 @@ void create() {
 
   short_desc = "Cracked Silted Narrow Way";
   long_desc = "The ground is a mosaic of cracked slabs and loose grit, dulled by age. The remains of pillars stand jagged, split and fused by old force. The way runs on in a narrow line, hemmed by broken walls left to weather. Thin lichen mats the shaded edges, and the air tastes of wet dust.";
+  /*
   dest_dir = ([
     "north" : "/chapter/prologue/area/dead/room1643",
     "south" : "/chapter/prologue/area/dead/room1510",
   ]);
+  */
 }
